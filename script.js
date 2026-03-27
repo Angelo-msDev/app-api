871d55e2feb3fe638eb254ba076bc27544deeef1
Angelo-msDev <angelo.mascarenhas.de.souza@gmail.com>
2026-03-26 21:06:13 -0300

    feat: estrutura completa da interface mobile e integração inicial com API
    Criação do esqueleto HTML com foco em acessibilidade e IDs para manipulação via JS.
    
    Estilização CSS Mobile-First: fundo dark, cores neon (Ben 10) e componentes responsivos.
    
    Implementação de cards com efeitos de borda e centralização de elementos (Logo/Input).
    
    Configuração inicial do script.js com função assíncrona (async/await) para consumo da API.

---
+++ script.js
@@ -0,0 +1,17 @@
+async function carregarAliens() {
+    try {
+        const url = 'https://ben10api.vercel.app/api/aliens'; // URL da Vercel
+        const resposta = await fetch(url);
+        
+        if (!resposta.ok) throw new Error("Erro ao carregar a API");
+
+        const dados = await resposta.json();
+        console.log("Dados recebidos:", dados);
+        
+        // Se chegar aqui, o console vai brilhar com os aliens!
+    } catch (erro) {
+        console.error("Ops, algo deu errado:", erro);
+    }
+}
+
+carregarAliens();
